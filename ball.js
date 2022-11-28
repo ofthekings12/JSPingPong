93e619e5e07f0e93a489cab71194f42db04cd9a1
Mikko De Los Reyes <mikkode212@gmail.com>
2022-11-28 15:40:21 -0500

    creating playerpaddle

--- ball.js
+++ ball.js
@@ -1,3 +1,6 @@
+const INITIAL_VELOCITY = 0.025
+const VELOCITY_INCREASE = 0.00001
+
 export default class Ball {
   constructor (ballElem) {
     this.ballElem = ballElem
@@ -19,23 +22,38 @@ export default class Ball {
     this.ballElem.style.setProperty("--y", value);
   }
 
+  rect() {
+    return this.ballElem.getBoundingClientRect()
+  }
+
   reset() {
     this.x = 50
     this.y = 50
-    this.direction = { x: 0.75, y: 0.5 }
+    this.direction = { x: 0 }
     while(
-      Math.abs(this.direction.x) <= .2 || 
-      Math.abs(this.direction.x) >= .9
+      Math.abs(this.direction.x) <= 0.2 || 
+      Math.abs(this.direction.x) >= 0.9
       ) {
       const heading = randomNumberBetween(0, 2 * Math.PI)
       this.direction = { x: Math.cos(heading), y: Math.sin(heading) }
     }
-    console.log(this.direction)
+    this.velocity = INITIAL_VELOCITY
+   
   }
 
   update(delta) {
-    this.x = 5
-    this.y = 15
+    this.x += this.direction.x * this.velocity * delta
+    this.y += this.direction.y * this.velocity * delta
+    const rect = this.rect()
+    this.velocity += VELOCITY_INCREASE * delta
+
+    if(rect.bottom >= window.innerHeight || rect.top <= 0) {
+      this.direction.y *= -1
+    }
+
+    if(rect.left >= window.innerWidth || rect.left <= 0) {
+      this.direction.x *= -1
+    }
   }
 }
 
--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 // Update Loop (updates position and logic of all the pieces in the game)
-
-import Ball from '/Ball.js';
+import Ball from './Ball.js';
 
 const ball = new Ball(document.getElementById("ball"))
+const playerPaddle = new Paddle(document.getElementById("player-paddle"))
+const computerPaddle = new Paddle(document.getElementById("computer-paddle"))
 
 let lastTime
 function update(time) {
   if (lastTime != null) {
     const delta = time - lastTime;
     // Update Code
-    ball.update(delta)
+    // ball.update(delta)
   }
   
   lastTime = time;
